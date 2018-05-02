bd0896f1917a514867517000130900f8d4007efc
jkest90 <jkest618@gmail.com>
2018-05-01 19:15:49 -0600

    checkpoint/assignment 4-3: complete

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,8 @@ class RoomList extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         rooms: []
+         rooms: [],
+         newChatRoom: ''
       };
       this.roomsRef = this.props.firebase.database().ref('rooms');
    }
@@ -14,13 +15,32 @@ class RoomList extends Component {
          const room = snapshot.val();
          room.key = snapshot.key;
          this.setState({ rooms: this.state.rooms.concat(room) });
-         console.log('child added!', room, room.key);
+         console.log('child added!', room);
       });
+   }
+
+   handleChange(e) {
+      this.setState({ newChatRoom: e.target.value })
+   }
+
+   createRoom(e) {
+      e.preventDefault();
+      // if (!this.state.newChatRoom) { return }
+      // const newRoom = { roomName: this.state.newChatRoom }
+      this.roomsRef.push({
+         name: this.state.newChatRoom
+      });
+      console.log('New Room Added!')
    }
 
    render() {
       return (
          <div>
+            <form onSubmit={ (e) => this.createRoom(e) }>
+               <label> Create A Chat Room </label>
+               <input type="text" value={ this.state.newChatRoom } onChange={ (e) => this.handleChange(e) } placeholder="Choose a Room Name" />
+               <input type="submit" />
+            </form>
             {
                this.state.rooms.map( (room, index) =>
                   <div key={room.key}>
@@ -34,3 +54,10 @@ class RoomList extends Component {
 }
 
 export default RoomList;
+
+
+// Checkpoint 4.4 - Create a Chat Room
+// Render a form with a text input and a submit input.
+// Whenever there is a change to the input, call, handleChange via onChange. handleChange(e) allows to set the value of this.state.newChatRoom to whatever our input is.
+// When we click submit, we call createRoom(e) onSubmit. This pushes the value of this.state.newChatRoom into our database reference, which ultimately is added to our state's
+// rooms array, mapped over, and displayed on our page.
